97a722afde957a309e3eea59f9fcc66a30e930f0
Daniel Schock <daniel.schock@glinnex.com>
2021-02-01 21:40:42 +0100

    github pages ready fix

--- next.config.js
+++ next.config.js
@@ -1,30 +1,7 @@
-const withSass = require("@zeit/next-sass");
-const withCss = require('@zeit/next-css')
-const withFonts = require("next-fonts");
-
-// noinspection JSUnusedLocalSymbols
-module.exports =
-  withFonts(
-    withSass(
-      withCss({
-        webpack: function (config) {
-          config.module.rules.push({
-            test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-            use: {
-              loader: 'url-loader',
-              options: {
-                limit: 100000,
-                name: '[name].[ext]',
-                mimeType: true
-              }
-            }
-          })
-          return config
-        },
-        exportPathMap: function() {
-          return {
-            '/booty': { page: '/' },
-          }
-        },
-      }))
-  );
+module.exports = {
+  exportPathMap: async function () {
+    return {
+      '/booty': { page: '/' },
+    }
+  },
+}
