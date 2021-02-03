b70617fe964d7fef7c7687d391093eb50c1d0c26
TieEagle <26168249+TieEagle@users.noreply.github.com>
2021-02-03 14:15:29 +0000

    Deploying to pages from  @ 4b4363df463ca014f7b6d4c4803611781573253e 🚀

---
+++ _next/static/_pi9j0UsulTiMPu9zXkSj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fc13a3c0e65afc413602e.css","static\u002Fchunks\u002Fpages\u002Findex-9d45aaa0ced318bafd64.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-6e43aa875d077fb3f93c.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
